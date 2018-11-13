e28801994daa79320d8950ec17ac397561ad016e
Daniel J <danieldrdj01@gmail.com>
2018-11-12 18:43:33 -0900

    update

--- JohnsonDaniel_HW4/src/Calculator.java
+++ JohnsonDaniel_HW4/src/Calculator.java
@@ -1,11 +1,12 @@
+import java.awt.*;
 import java.io.*;
-import java.awt.FlowLayout;
 import javax.swing.*;
+import java.util.Scanner;
 
 
 
 
-public class Calculator {
+public class Calculator extends Frame{
 
 
 
@@ -16,10 +17,50 @@ public class Calculator {
 
     public static void main(String[] args)
     {
-    JLabel = new JLabel;
+        Frame frame = new Frame();
+        JPanel panel  = new JPanel(new GridLayout());
+        //GridBagConstraints constraints = new GridBagConstraints();
+        //constraints.anchor = GridBagConstraints.NORTH;
+        //constraints.insets = new Insets(50,50,50,50);
+        frame.setSize(new Dimension(100,100));
 
 
 
+        Button button1 = new Button("1");
+        Button button2 = new Button("2");
+        Button button3 = new Button("3");
+        Button button4 = new Button("4");
+        Button button5 = new Button("5");
+
+        button1.setSize(50,50);
+        button2.setSize(50,50);
+        button3.setSize(50,50);
+        button4.setSize(50,50);
+        button5.setSize(50,50);
+
+        panel.add(button1);
+        panel.add(button2);
+        panel.add(button3);
+        panel.add(button4);
+        panel.add(button5);
+
+
+
+        frame.add(panel);
+        frame.setVisible(true);
+        panel.setVisible(true);
+
+
+
+
+
+
+
+
+
+
+    Scanner sc = new Scanner(System.in);
+    sc.next();
 
 
     }
